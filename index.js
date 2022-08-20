2daf40dc8e8618077e727b755e9eeaf5a28985d1
gombouka <Tomondup266>
2022-08-20 10:26:16 +0200

    Coiffeur bot : wordlist optimisation

--- index.js
+++ index.js
@@ -13,25 +13,33 @@ const client = new Client( {
 	partials: partials,
 } );
 
-let guild;
-
 /** On startup */
 client.once( 'ready', () => {
 	console.log( 'Bot started' );
 } );
 
+let wordlist = {
+	'quoi': 'feur',
+	'non': 'brill',
+	'ca va': 'ba oe et toi mon reuf',
+	'ouais': 'stern',
+	'oui': 'stiti',
+	'wesh': 'den',
+	'question': 'pause les direct mon reuf',
+};
+
 /** On message */
 client.on( 'messageCreate', async message => {
+	
 	if (message.author.bot) return;
 	//if (message.channel.id === process.env.CHANNEL_ID) {
 	const guild = message.guild;
 	const content = message.content.toLowerCase();
-	if (content.slice( -7 ).includes( 'quoi' )) {
-		await message.reply( 'feur' );
-		//}
-		
+	for (const key in wordlist) {
+		if (content.slice( -key.length - 2 ).includes( key )) {
+			await message.reply( wordlist[key] );
+		}
 	}
-	
 } );
 
-client.login( process.env.TOKEN ); // Your Token
+client.login( process.env.TOKEN );
